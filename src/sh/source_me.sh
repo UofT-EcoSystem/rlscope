ef6d366e1e13fa1270e940fae5d10ad277d941e4
James Gleeson <jagleeso@gmail.com>
2019-04-09 15:02:09 -0400

    Make sure things still "work", before we try to add async profiler-data-dumping needed for "root annotations".

---
+++ src/sh/source_me.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+export IML=$HOME/clone/dnn_tensorflow_cpp
+
+_activate_virtualenv_cuda9() {
+    source $HOME/envs/cuda9/bin/activate
+}
+
+_activate_cuda9() {
+export PATH="\
+/usr/local/cuda-9.0/bin:\
+$HOME/bin:\
+$PATH"
+export LD_LIBRARY_PATH="\
+/usr/local/cuda-9.0/lib64:\
+/usr/local/cuda-9.0/extras/CUPTI/lib64:\
+$LD_LIBRARY_PATH"
+}
+
+_activate_virtualenv_cuda9
+_activate_cuda9
