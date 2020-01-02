cb5d74691a888adab321b82a3c1070dd1dd7be24
James <jagleeso@gmail.com>
2020-01-02 18:11:59 -0500

    .

---
+++ run_dbg.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# read -r -d '' BASH_SCRIPT << EOM
+# cd \$IML_DIR;
+# $@;
+# EOM
+#
+# docker exec --user $USER -i -t dockerfiles_bash_1 /bin/bash -c "$BASH_SCRIPT"
+set -e
+if [ "$DEBUG" = 'yes' ]; then
+    set -x
+fi
+cd $(dirname $0)
+./run.sh "cd Debug && make -j$(nproc)" 
+./run.sh "$@"
+# Debug/cpp_dump_proto --mode overlap --iml_directory output/perf_debug
